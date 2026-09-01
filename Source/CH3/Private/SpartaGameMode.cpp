b378e75ef26a4ae6970b1263a712554cdeabf780
CherryCereal <a2002seo@gmail.com>
2026-09-01 19:55:05 +0900

    Reapply "Ch3_HW3 init"
    This reverts commit 1bcef5fe43b9674aabbfe036f9a50534f521572e.

--- Source/CH3/Private/SpartaGameMode.cpp
+++ Source/CH3/Private/SpartaGameMode.cpp
@@ -2,9 +2,10 @@
 #include "SpartaCharacter.h"
 #include "HWPawn.h"
 #include "SpartaPlayerController.h"
-
+#include "SpartaGameState.h"
 ASpartaGameMode::ASpartaGameMode()
 {
 	DefaultPawnClass = AHWPawn::StaticClass();
 	PlayerControllerClass = ASpartaPlayerController::StaticClass();
+	GameStateClass = ASpartaGameState::StaticClass();
 }
---
+++ Source/CH3/Public/BaseActor.h
@@ -0,0 +1,54 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemInterface.h"
+#include "BaseActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class CH3_API ABaseActor : public AActor, public IItemInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseActor();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FName ItemType;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USceneComponent* Scene;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USphereComponent* Collision;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effects")
+	UParticleSystem* PickUpParticle;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effects")
+	USoundBase* PickUpSound;
+
+	virtual void OnItemOverlap(UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult) override;
+
+	virtual void OnItemEndOverlap(UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex) override;
+	virtual void ActivateItem(AActor* Activator) override;
+	virtual FName GetItemType() const override;
+
+	virtual void DestroyItem();
+
+	
+
+};
